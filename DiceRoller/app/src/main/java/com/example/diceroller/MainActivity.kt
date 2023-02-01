1b36b9bce506f472ba72af999921335ab2240844
Peter Njuguna <petermwaurapeter254@gmail.com>
2023-02-01 23:12:48 +0300

    Completed Dice Roller app codelab

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +15,17 @@ class MainActivity : AppCompatActivity() {
     }
     private fun rolldice() {
         val mydice = RoleDice(6)
-        val rolloutput: TextView = findViewById(R.id.textView)
-        rolloutput.text = mydice.rollDice().toString()
+        val diceimage: ImageView = findViewById(R.id.imageView)
+        val rollResults:Int = mydice.rollDice()
+        when(rollResults)
+        {
+            1->diceimage.setImageResource(R.drawable.dice_1)
+            2->diceimage.setImageResource(R.drawable.dice_2)
+            3->diceimage.setImageResource(R.drawable.dice_3)
+            4->diceimage.setImageResource(R.drawable.dice_4)
+            5->diceimage.setImageResource(R.drawable.dice_5)
+            6->diceimage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 class RoleDice(val numofSides: Int) {
